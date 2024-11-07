6289a9c1a5e00fd76efc3e32aafa17614f14274c
Jabin0214 <jabinchen0214@gmail.com>
2024-11-07 13:47:08 +1300

    new page

--- 168diary/168diary/ContentView.swift
+++ 168diary/168diary/ContentView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
-
     var body: some View {
         VStack {
+            // 优化标题
             Text("168 Diary")
-                .font(.title)
-            
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundStyle(LinearGradient(
+                    gradient: Gradient(colors: [Color.blue, Color.purple]),
+                    startPoint: .leading,
+                    endPoint: .trailing
+                ))
+                .padding()
+                .shadow(color: .gray.opacity(0.5), radius: 5, x: 0, y: 3)
+
             TabView {
                 // 第一个标签页：主页
                 Text("Home")
@@ -21,14 +29,22 @@ struct ContentView: View {
                         Image(systemName: "house.fill")
                         Text("Home")
                     }
-                
+
                 // 第二个标签页：计时器视图
                 TimerView()
                     .tabItem {
                         Image(systemName: "timer")
                         Text("Time")
                     }
+
+                // 第三个标签页：喝水页面
+                Text("Water")
+                    .tabItem {
+                        Image(systemName: "drop.fill")
+                        Text("Water")
+                    }
             }
+            .accentColor(.purple) // 更改选中状态的颜色
         }
     }
 }
